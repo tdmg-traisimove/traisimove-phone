a5eb29528824242900f6a0c6fe4ae8aa027155c4
Io A. Daza-Dillon <io@dazadillon.io>
2024-11-11 10:44:47 -0500

    Use own privacy policy

--- www/js/onboarding/PrivacyPolicy.tsx
+++ www/js/onboarding/PrivacyPolicy.tsx
@@ -54,152 +54,69 @@ const PrivacyPolicy = () => {
 
   return (
     <>
-      <Text style={styles.title}>{t('consent-text.title')}</Text>
-      <Text style={styles.header}>{t('consent-text.introduction.header')}</Text>
-      <Text style={styles.text}>{templateText?.short_textual_description}</Text>
+      <Text style={styles.title}>Term of use:</Text>
+      <Text style={styles.header}>Introduction &amp; Purpose</Text>
+      <Text style={styles.text}>Collecting GPS data via mobile devices is essential to understanding human mobility patterns.
+This data enables researchers to analyze travel behavior with exceptional detail and accuracy,
+supports informed infrastructure planning, provides real-time insights into mobility trends, and
+facilitates the development of sustainable urban environments. To achieve these objectives, this
+study requests that participants allow the TRAISI Move application (the “App”) on their
+smartphones to collect travel history data.</Text>
       <Text>{'\n'}</Text>
-      <Text style={styles.text}>{t('consent-text.introduction.what-is-openpath')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.introduction.what-is-NREL', {
-          program_or_study: appConfig?.intro?.program_or_study,
-        })}
-      </Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>{t('consent-text.introduction.if-disagree')}</Text>
+      <Text style={styles.text}>If you do not consent to the terms outlined in this Privacy Policy, please delete the App.</Text>
       <Text>{'\n'}</Text>
 
-      <Text style={styles.header}>{t('consent-text.why.header')}</Text>
-      <Text style={styles.text}>{templateText?.why_we_collect}</Text>
+      <Text style={styles.header}>Purpose of Travel Survey Data Collection</Text>
+      <Text style={styles.text}>The data collected will support research into mobility patterns, route choice modeling, transport
+mode identification, trip origin-destination purposes, and analyses of urban passenger activity-
+travel behavior. Insights gained from this survey will assist researchers, policymakers, and
+planners in the Greater Toronto and Hamilton Area (GTHA) in making evidence-based policy
+and infrastructure planning decisions related to transportation.</Text>
       <Text>{'\n'}</Text>
 
-      <Text style={styles.header}>{t('consent-text.what.header')}</Text>
-      <Text style={styles.text}>{t('consent-text.what.no-pii')}</Text>
+      <Text style={styles.header}>Information We Collect</Text>
+      <Text style={styles.text}>The App does not request or store Personally Identifiable Information (PII), such as your name
+or phone number. Instead, it collects mobile device sensor data related to your location
+(including background location), accelerometer readings, device-generated activity and mode
+recognition, App usage duration, and battery consumption. The App will generate a “travel
+diary” based on location data to help determine travel patterns and history.</Text>
       <Text>{'\n'}</Text>
-      <Text style={styles.text}>{t('consent-text.what.phone-sensor')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>{t('consent-text.what.labeling')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>{t('consent-text.what.demographics')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.what.open-source-data')}
-        <Text
-          style={styles.hyperlinkStyle(colors.primary)}
-          onPress={() => {
-            window['cordova'].InAppBrowser.open(
-              'https://github.com/e-mission/e-mission-data-collection.git',
-              '_system',
-            );
-          }}>
-          {' '}
-          https://github.com/e-mission/e-mission-data-collection.git{' '}
-        </Text>
-        {t('consent-text.what.open-source-analysis')}
-        <Text
-          style={styles.hyperlinkStyle(colors.primary)}
-          onPress={() => {
-            window['cordova'].InAppBrowser.open(
-              'https://github.com/e-mission/e-mission-server.git',
-              '_system',
-            );
-          }}>
-          {' '}
-          https://github.com/e-mission/e-mission-server.git{' '}
-        </Text>
-        {t('consent-text.what.open-source-dashboard')}
-        <Text
-          style={styles.hyperlinkStyle(colors.primary)}
-          onPress={() => {
-            window['cordova'].InAppBrowser.open(
-              'https://github.com/e-mission/em-public-dashboard.git',
-              '_system',
-            );
-          }}>
-          {' '}
-          https://github.com/e-mission/em-public-dashboard.git.{' '}
-        </Text>
-      </Text>
+      <Text style={styles.text}>You may also be asked to annotate these trips periodically with semantic labels, such as trip
+mode, purpose, and alternative modes. Additionally, the App will request general
+sociodemographic information such as approximate age, gender, and household type, which
+helps contextualize travel behavior and enables generalization of results to broader populations.</Text>
       <Text>{'\n'}</Text>
 
-      <Text style={styles.header}>{t('consent-text.opcode.header')}</Text>
-      {opCodeText}
+      <Text style={styles.header}>Data Access and Privacy Protections</Text>
+      <Text style={styles.text}>This survey is conducted anonymously and will not collect any information that could directly
+identify you or your family, such as names or unique identifiers. The App collects the location of
+your home and trip destinations; however, this data will be aggregated into larger geographical
+units (e.g., census tracts) before being made available for analysis to prevent identification of
+specific locations.</Text>
+      <Text>{'\n'}</Text>
+      <Text style={styles.text}>Detailed location data will be securely stored at the University of Toronto (UofT) indefinitely
+and will only be used for research purposes under the supervision of Professor Khandker Nurul
+Habib. Any results from analyses will be reported in an aggregated format only. None of the data
+collected by the App will be sold or used for commercial purposes, including advertising.</Text>
       <Text>{'\n'}</Text>
 
-      <Text style={styles.header}>{t('consent-text.who-sees.header')}</Text>
-      <Text style={styles.text}>{t('consent-text.who-sees.public-dash')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>{t('consent-text.who-sees.individual-info')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.who-sees.program-admins', {
-          deployment_partner_name: appConfig?.intro?.deployment_partner_name,
-          raw_data_use: templateText?.raw_data_use,
-        })}
-      </Text>
-      <Text style={styles.text}>{t('consent-text.who-sees.nrel-devs')}</Text>
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.who-sees.TSDC-info')}
-        <Text
-          style={styles.hyperlinkStyle(colors.primary)}
-          onPress={() => {
-            window['cordova'].InAppBrowser.open(
-              'https://www.nrel.gov/transportation/secure-transportation-data/',
-              '_system',
-            );
-          }}>
-          {t('consent-text.who-sees.on-website')}
-        </Text>
-        {t('consent-text.who-sees.and-in')}
-        <Text
-          style={styles.hyperlinkStyle(colors.primary)}
-          onPress={() => {
-            window['cordova'].InAppBrowser.open(
-              'https://www.sciencedirect.com/science/article/pii/S2352146515002999',
-              '_system',
-            );
-          }}>
-          {t('consent-text.who-sees.this-pub')}
-        </Text>
-        {t('consent-text.who-sees.and')}
-        <Text
-          style={styles.hyperlinkStyle(colors.primary)}
-          onPress={() => {
-            window['cordova'].InAppBrowser.open(
-              'https://www.nrel.gov/docs/fy18osti/70723.pdf',
-              '_system',
-            );
-          }}>
-          {t('consent-text.who-sees.fact-sheet') + '.'}
-        </Text>
-      </Text>
+      <Text style={styles.header}>Your Rights</Text>
+      <Text style={styles.text}>Participation in this study is entirely voluntary. You have the right to decline participation or
+withdraw from the study at any time without notice. To withdraw from the study, you may
+simply delete the App from your device.</Text>
       <Text>{'\n'}</Text>
 
-      <Text style={styles.header}>{t('consent-text.rights.header')}</Text>
-      {yourRightsText}
-      <Text>{'\n'}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.rights.destroy-data-pt1')}
-        <Text>{'(k.shankari@nrel.gov)'}</Text>
-        {t('consent-text.rights.destroy-data-pt2')}
-      </Text>
+      <Text style={styles.header}>Questions</Text>
+      <Text style={styles.text}>If you have questions or require further information regarding this research, please contact the
+      research team:</Text>
+      <Text style={styles.text}>Professor Khandker Nurul Habib</Text>
+      <Text style={styles.text}>Tel: 416-946-8027</Text>
+      <Text style={styles.text}>Email: khandker.nurulhabib@utoronto.ca</Text>
       <Text>{'\n'}</Text>
 
-      <Text style={styles.header}>{t('consent-text.questions.header')}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.questions.for-questions', {
-          program_admin_contact: appConfig?.intro?.program_admin_contact,
-        })}
-      </Text>
-      <Text>{'\n'}</Text>
-
-      <Text style={styles.header}>{t('consent-text.consent.header')}</Text>
-      <Text style={styles.text}>
-        {t('consent-text.consent.press-button-to-consent', {
-          program_or_study: appConfig?.intro?.program_or_study,
-        })}
-      </Text>
+      <Text style={styles.header}>Consent</Text>
+      <Text style={styles.text}>Please select the button below to indicate that you have read, understood, and agree to this
+      Privacy Policy, consent to the collection of your information, and wish to participate in the study.</Text>
     </>
   );
 };
