b5ceaee78f9f6768dd7c9342298111dfe67611cc
MelvynLi <melvyn.li@utoronto.ca>
2026-06-12 16:08:48 -0400

    Update dynamicConfig.ts
    deploy config url

--- www/js/config/dynamicConfig.ts
+++ www/js/config/dynamicConfig.ts
@@ -112,7 +112,7 @@ async function readConfigFromServer(studyLabel: string) {
  */
 async function fetchConfig(studyLabel: string, alreadyTriedLocal?: boolean) {
   logDebug('Received request to join ' + studyLabel);
-  let downloadURL = `https://raw.githubusercontent.com/e-mission/op-deployment-configs/main/configs/${studyLabel}.nrel-op.json`;
+  let downloadURL = `https://raw.githubusercontent.com/tdmg-traisimove/traisimove-deploy-configs/main/configs/${studyLabel}.nrel-op.json`;
   if (!__DEV__ || alreadyTriedLocal) {
     logDebug('Fetching config from github');
     const r = await fetch(downloadURL, { cache: 'reload' });
